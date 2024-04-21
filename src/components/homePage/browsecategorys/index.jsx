d0c2e3e5dd4537c0ee0d27692755e6cd5b8b43bb
Shekhri <khamidovashakhrezada3868@gmail.com>
2024-04-21 14:36:47 +0500

    home-categories

--- src/components/homePage/browsecategorys/index.jsx
+++ src/components/homePage/browsecategorys/index.jsx
@@ -1,10 +1,75 @@
 import React from "react";
 import styles from './style.module.css'
+import img1 from './image/art.png'
+import img2 from './image/Collectibles.png'
+import img3 from './image/music.png'
+import img4 from './image/Photography.png'
+import img5 from './image/video.png'
+import img6 from './image/Utility.png'
+import img7 from './image/video.png'
+import img8 from './image/VirtualWorlds.png'
 
 function BrowseCategory() {
-   return(
+   return (
       <>
-         <h1>BrowseCategory</h1>
+         <div className={styles.BrowseCategories} >
+            <div className={styles.container}>
+               <div className={styles.categories}>
+                  <h2 className={styles.h2}>Browse Categories</h2>
+                  <div className={styles.card}>
+                     <div className={styles.box}>
+                        <img className={styles.img} src={img1} alt="categories image" />
+                        <p className={styles.p}>
+                           Art
+                        </p>
+                     </div>
+                     <div className={styles.box}>
+                        <img className={styles.img} src={img2} alt="categories image" />
+                        <p className={styles.p}>
+                           Collectibles
+                        </p>
+                     </div>
+                     <div className={styles.box}>
+                        <img className={styles.img} src={img3} alt="categories image" />
+                        <p className={styles.p}>
+                           Music
+                        </p>
+                     </div>
+                     <div className={styles.box}>
+                        <img className={styles.img} src={img4} alt="categories image" />
+                        <p className={styles.p}>
+                           Photography
+                        </p>
+                     </div>
+                     <div className={styles.box}>
+                        <img className={styles.img} src={img5} alt="categories image" />
+                        <p className={styles.p}>
+                           Video
+                        </p>
+                     </div>
+                     <div className={styles.box}>
+                        <img className={styles.img} src={img6} alt="categories image" />
+                        <p className={styles.p}>
+                           Utility
+                        </p>
+                     </div>
+                     <div className={styles.box}>
+                        <img className={styles.img} src={img7} alt="categories image" />
+                        <p className={styles.p}>
+                           Sport
+                        </p>
+                     </div>
+                     <div className={styles.box}>
+                        <img className={styles.img} src={img8} alt="categories image" />
+                        <p className={styles.p}>
+                           Virtual Worlds
+                        </p>
+                     </div>
+
+                  </div>
+               </div>
+            </div>
+         </div>
       </>
    )
 }
