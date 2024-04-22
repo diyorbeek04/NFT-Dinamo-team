8bc40d4c96aea05d4e2cdaa91ad47d0cc4a430ce
sam <156315526+samisuser@users.noreply.github.com>
2024-04-22 14:22:03 +0500

    oxirgisi rangiga tegmadim

--- src/pages/artistPage/index.jsx
+++ src/pages/artistPage/index.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import ArtistHero from "../../components/artistPage/artistHero";
-import AnimakidComponent from "../../components/artistPage/animakid";
 import ArtistCardsComponent from "../../components/artistPage/artistCards";
 
 function ArtistPage() {
    return(
       <>
          <ArtistHero/>
-         <AnimakidComponent/>
          <ArtistCardsComponent/>
       </>
    )
